12f73db1d2647704c85415dea52ce14ca08a9664
LiangYaHui <46517115@qq.com>
2021-09-24 16:44:28 +0800

    去掉logs忽略

--- example/app.js
+++ example/app.js
@@ -7,24 +7,6 @@ import {
 } from './wxlsp/index'
 
 (function (wx, App, LspApp, StartApp) {
-  // App({
-  //   onLaunch() {
-  //     // 展示本地存储能力
-  //     const logs = wx.getStorageSync('logs') || []
-  //     logs.unshift(Date.now())
-  //     wx.setStorageSync('logs', logs)
-
-  //     // 登录
-  //     wx.login({
-  //       success: res => {
-  //         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-  //       }
-  //     })
-  //   },
-  //   globalData: {
-  //     userInfo: null
-  //   }
-  // })
   class Application extends LspApp {
     constructor() {
       super()
